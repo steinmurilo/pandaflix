d567024e9a3292e28ff4b4d01f63bcb8e1722952
murilo <murilo.stein@atmtec.com.br>
2020-07-27 21:10:56 -0300

    primeira aula imersao react alura

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png'
+import './menu.css'
+import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Pandaflix" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu
